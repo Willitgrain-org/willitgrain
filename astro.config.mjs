d206b69bf7b3aa8d603322666dbeb58c67b71f1d
Lucas Kolinger <kolingerlucas@gmail.com>
2026-05-16 23:53:55 +0200

    Fix Netlify build SSR adapter for Keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 import keystatic from "@keystatic/astro"
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +12,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   integrations: [react(), keystatic()],
+  adapter: netlify(),
 })
